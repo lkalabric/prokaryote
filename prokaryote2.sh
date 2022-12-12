5d924eb748f18e7b1ff3b524dec69e53aa7ec29a
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-12-12 10:07:20 -0300

    Update prokaryote2.sh

--- prokaryote2.sh
+++ prokaryote2.sh
@@ -101,6 +101,7 @@ function trim_bper () {
 					${TRIMMOMATICDIR}/${LIBNAME}_R2.fastq ${TRIMMOMATICDIR}/${LIBNAME}_R2u.fastq \
 					SLIDINGWINDOW:4:20 MINLEN:35
 		
+		cat ${TRIMMOMATICDIR}/${LIBNAME}_R1u.fastq ${TRIMMOMATICDIR}/${LIBNAME}_R2u.fastq > ${TRIMMOMATICDIR}/${LIBNAME}_R1R2u.fastq
 		# Original code
 		# trimmomatic PE -threads ${THREADS} -trimlog ${TRIMMOMATICDIR}/${LIBNAME}_trimlog.txt \
 		#			-summary ${TRIMMOMATICDIR}/${LIBNAME}_summary.txt \
@@ -180,7 +181,7 @@ function spades_bper () {
 		
 		# New
 		spades.py -1 ${IODIR}/*R1*.fastq* -2 ${IODIR}/*R2*.fastq* \
-			-s ${IODIR}/*R1u.fastq* -2 ${IODIR}/*R2u.fastq* \
+			-s ${IODIR}/*R1R2u.fastq* \
 			--only-assembler --careful -o ${SPADESDIR}
 		# Original 
 		# Verifica o número de arquivos em ${IODIR}
