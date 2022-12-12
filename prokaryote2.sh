3fa6b2772ff27edf76fd74a41680cd5edcd6729b
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-12-12 10:09:59 -0300

    Update prokaryote2.sh

--- prokaryote2.sh
+++ prokaryote2.sh
@@ -181,7 +181,6 @@ function spades_bper () {
 		
 		# New
 		spades.py -1 ${IODIR}/*R1*.fastq* -2 ${IODIR}/*R2*.fastq* \
-			-s ${IODIR}/*R1R2u.fastq* \
 			--only-assembler --careful -o ${SPADESDIR}
 		# Original 
 		# Verifica o número de arquivos em ${IODIR}
