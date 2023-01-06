a565cb01e810ce1031bfa426350a17ccbdb727de
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-01-06 15:29:00 -0300

    Update fastq2a.sh

--- fastq2a.sh
+++ fastq2a.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# prokaryote.sh
+# Workflow de bioinformática para análise de Bordetella pertussis
+# Autor: Luciano Kalabric & Viviane Ferreira
+# Objetivo: Análise das sequencias, filtro de qualidade, correção das reads e montagem de novo
+# Controle de versão: Incluir o log das próximas atualizações aqui
+# Versão 1.0 de 07 DEZ 2022 - Programa inicial revisado
+
+# Requirements: bbmap, fastqc, conda, trimmomatic, musket, flash, khmer, spades
+
+# Entrada de dados
+RESULTSDIR=ngs-library/
+
 FILENAME=`echo "$1" | cut -d'.' -f1`
 
+# Converte fastq para fasta
 sed -n '1~4s/^@/>/p;2~4p' "${FILENAME}.fastq" > "${FILENAME}.fasta"
+
+# Conta o número de Ns
+grep -c "N" "${FILENAME}.fasta"
