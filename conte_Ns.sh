4d2372150c0bd9f554d60995d4b2b5c20b038aa1
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-01-06 16:23:03 -0300

    Update conte_Ns.sh

--- conte_Ns.sh
+++ conte_Ns.sh
@@ -18,22 +18,35 @@ if [[ $# -ne 2 ]]; then
 	exit 0
 fi
 
-RESULTSDIR="/media/brazil1/Seagate Expansion Drive/ngs-analysis/${LIBNAME}/wf${WF}/KHMER"
+KHMERDIR="/media/brazil1/Seagate Expansion Drive/ngs-analysis/${LIBNAME}/wf${WF}/KHMER"
+SPADESDIR="/media/brazil1/Seagate Expansion Drive/ngs-analysis/${LIBNAME}/wf${WF}/SPADES"
 LOGDIR="/media/brazil1/Seagate Expansion Drive/ngs-analysis/${LIBNAME}/wf${WF}"
 
-# FILENAME=`echo "$1" | cut -d'.' -f1`
-
+# Dados do KHMER
 # Converte fastq para fasta
-sed -n '1~4s/^@/>/p;2~4p' "${RESULTSDIR}/${LIBNAME}.fastq" > "${RESULTSDIR}/${LIBNAME}.fasta"
+sed -n '1~4s/^@/>/p;2~4p' "${KHMERDIR}/${LIBNAME}.fastq" > "${KHMERDIR}/${LIBNAME}.fasta"
 
 # Total de reads
 echo -e "\nTotal de reads:" >> "${LOGDIR}/${LIBNAME}_report.log"
-echo -e "$(grep -c ">" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c ">" "${KHMERDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
 
 # Total de base
 echo "Total de bases:" >> "${LOGDIR}/${LIBNAME}_report.log"
-echo -e "$(grep -v ">" "${RESULTSDIR}/${LIBNAME}.fasta" | wc -m)\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -v ">" "${KHMERDIR}/${LIBNAME}.fasta" | wc -m)\n" >> "${LOGDIR}/${LIBNAME}_report.log"
 
 # Conta o número de Ns
 echo "Total de Ns:" >> "${LOGDIR}/${LIBNAME}_report.log"
-echo -e "$(grep -c "N" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c "N" "${KHMERDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+
+# Dados do SPADES
+# Total de contigs
+echo -e "\nTotal de contigs:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c ">" "${SPADESDIR}/contigs.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+
+# Total de base
+echo "Total de bases:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -v ">" "${SPADESDIR}/contigs.fasta" | wc -m)\n" >> "${LOGDIR}/${LIBNAME}_report.log"
+
+# Conta o número de Ns
+echo "Total de Ns:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c "N" "${SPADESDIR}/contigs.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
