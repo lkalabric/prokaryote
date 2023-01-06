648b156c06a642e6affb3459b06ab106eb876b05
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2023-01-06 16:17:39 -0300

    Update conte_Ns.sh

--- conte_Ns.sh
+++ conte_Ns.sh
@@ -27,13 +27,13 @@ LOGDIR="/media/brazil1/Seagate Expansion Drive/ngs-analysis/${LIBNAME}/wf${WF}"
 sed -n '1~4s/^@/>/p;2~4p' "${RESULTSDIR}/${LIBNAME}.fastq" > "${RESULTSDIR}/${LIBNAME}.fasta"
 
 # Total de reads
-echo -e "\nTotal de reads:" >> ${LOGDIR}/${LIBNAME}_report.log
-echo -e "$(grep -c ">" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> ${LOGDIR}/${LIBNAME}_report.log
+echo -e "\nTotal de reads:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c ">" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
 
 # Total de base
-echo "Total de bases:" >> ${LOGDIR}/${LIBNAME}_report.log
-echo -e "$(grep -v ">" "${RESULTSDIR}/${LIBNAME}.fasta" | wc -m)\n" >> ${LOGDIR}/${LIBNAME}_report.log
+echo "Total de bases:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -v ">" "${RESULTSDIR}/${LIBNAME}.fasta" | wc -m)\n" >> "${LOGDIR}/${LIBNAME}_report.log"
 
 # Conta o número de Ns
-echo "Total de Ns:" >> ${LOGDIR}/${LIBNAME}_report.log
-echo -e "$(grep -c "N" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> ${LOGDIR}/${LIBNAME}_report.log
+echo "Total de Ns:" >> "${LOGDIR}/${LIBNAME}_report.log"
+echo -e "$(grep -c "N" "${RESULTSDIR}/${LIBNAME}.fasta")\n" >> "${LOGDIR}/${LIBNAME}_report.log"
